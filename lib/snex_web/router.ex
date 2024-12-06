cead1ed68ec215516869bc2ef751ee75e89d999b
Simon Madine <simon@komment.ai>
2024-12-06 09:03:29 +0000

    Remove some unused files

--- lib/snex_web/router.ex
+++ lib/snex_web/router.ex
@@ -20,24 +20,4 @@ defmodule SnakeGameWeb.Router do
     get "/", PageController, :home
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", SnakeGameWeb do
-  #   pipe_through :api
-  # end
-
-  # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:snex, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
-
-    scope "/dev" do
-      pipe_through :browser
-
-      live_dashboard "/dashboard", metrics: SnakeGameWeb.Telemetry
-    end
-  end
 end
